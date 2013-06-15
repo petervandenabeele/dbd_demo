29b250396d7a525c38f389efea79113c8b8f8894
Peter Vandenabeele <peter@vandenabeele.com>
2013-06-15 23:52:49 +0200

    rails new dbd_demo -JBOT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DbdDemo::Application.config.secret_key_base = 'c8a29bc9a48c6063d713e33644c1b62dff4c4706d3e2aa77805b4095b9d6ba99fdfff9f8f224727d3dd9f3108f7db106a07a39cea697e1ac140c566f5a5d8a4d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DbdDemo::Application.config.session_store :cookie_store, key: '_dbd_demo_session'

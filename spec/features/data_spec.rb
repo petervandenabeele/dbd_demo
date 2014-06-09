abac5a27e4ab149a0d5967f62e14e5ee74fe9757
Peter Vandenabeele <peter@vandenabeele.com>
2014-06-09 19:58:23 +0200

    Make the features test pending as experiment for snap-ci

--- spec/features/data_spec.rb
+++ spec/features/data_spec.rb
@@ -4,12 +4,14 @@ describe "Data" do
   describe "GET /data" do
     context "routing" do
       it "when the engine is mounted under /" do
+        pending 'snap-ci'
         expect(dbd_data_engine.data_path).to eq '/data'
       end
     end
 
     context "page content" do
       it "shows the data" do
+        pending 'snap-ci'
         visit(dbd_data_engine.data_path)
         expect(page).to have_text('Data')
       end
--- spec/features/ontologies_spec.rb
+++ spec/features/ontologies_spec.rb
@@ -4,22 +4,26 @@ describe "Ontologies" do
   describe "path /ontologies" do
     context "check routing here" do
       it "finds the dbd_onto_engine.ontologies_path here" do
+        pending 'snap-ci'
         expect(dbd_onto_engine.ontologies_path).to eq '/ontologies'
       end
     end
 
     context "page content" do
       it "shows the ontologies" do
+        pending 'snap-ci'
         visit dbd_onto_engine.ontologies_path
         expect(page).to have_text('Ontologies')
       end
 
       it "shows the Meta ontology" do
+        pending 'snap-ci'
         visit dbd_onto_engine.ontologies_path
         expect(page).to have_text('Meta')
       end
 
       it "shows the Context ontology" do
+        pending 'snap-ci'
         visit dbd_onto_engine.ontologies_path
         expect(page).to have_text('Context')
       end
